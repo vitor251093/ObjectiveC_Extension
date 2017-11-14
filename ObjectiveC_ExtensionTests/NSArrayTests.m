3d38f7348797d3b3fbe460edbf6b472cd21b7e2b
Vitor Marques de Miranda <vitor251093@yahoo.com.br>
2017-11-14 21:36:02 -0200

    Added some NSArray tests

--- ObjectiveC_ExtensionTests/NSArrayTests.m
+++ ObjectiveC_ExtensionTests/NSArrayTests.m
@@ -26,4 +26,69 @@
     XCTAssert([[input sortedDictionariesArrayWithKey:key orderingByValuesOrder:order] isEqualToArray:output]);
 }
 
+- (void)testArrayByRemovingRepetitions_noChange
+{
+    NSArray* originalArray = @[@1,@2,@3,@4,@5,@6];
+    NSArray* resultArray = [originalArray arrayByRemovingRepetitions];
+    
+    XCTAssert(resultArray.count == 6);
+    XCTAssert([resultArray containsObject:@1]);
+    XCTAssert([resultArray containsObject:@2]);
+    XCTAssert([resultArray containsObject:@3]);
+    XCTAssert([resultArray containsObject:@4]);
+    XCTAssert([resultArray containsObject:@5]);
+    XCTAssert([resultArray containsObject:@6]);
+}
+- (void)testArrayByRemovingRepetitions_fromSixToThreeResults
+{
+    NSArray* originalArray = @[@1,@2,@1,@2,@1,@4];
+    NSArray* resultArray = [originalArray arrayByRemovingRepetitions];
+    
+    XCTAssert(resultArray.count == 3);
+    XCTAssert([resultArray containsObject:@1]);
+    XCTAssert([resultArray containsObject:@2]);
+    XCTAssert([resultArray containsObject:@4]);
+}
+- (void)testArrayByRemovingRepetitions_oneResultOnly
+{
+    NSArray* originalArray = @[@1,@1,@1,@1,@1,@1];
+    NSArray* resultArray = [originalArray arrayByRemovingRepetitions];
+    
+    XCTAssert(resultArray.count == 1);
+    XCTAssert([resultArray containsObject:@1]);
+}
+
+- (void)testArrayByRemovingObjectsFromArray_removeTwoFromFive
+{
+    NSArray* originalArray = @[@1,@2,@3,@4,@5];
+    NSArray* itemsToRemove = @[@2,@5];
+    NSArray* result = [originalArray arrayByRemovingObjectsFromArray:itemsToRemove];
+    
+    XCTAssert(resultArray.count == 3);
+    XCTAssert([resultArray containsObject:@1]);
+    XCTAssert([resultArray containsObject:@3]);
+    XCTAssert([resultArray containsObject:@4]);
+}
+- (void)testArrayByRemovingObjectsFromArray_removeAll
+{
+    NSArray* originalArray = @[@1,@2,@3,@4,@5];
+    NSArray* itemsToRemove = @[@4,@3,@2,@1,@5];
+    NSArray* result = [originalArray arrayByRemovingObjectsFromArray:itemsToRemove];
+    
+    XCTAssert(resultArray.count == 0);
+}
+- (void)testArrayByRemovingObjectsFromArray_removeNothing
+{
+    NSArray* originalArray = @[@1,@2,@3,@4,@5];
+    NSArray* itemsToRemove = @[@7,@8];
+    NSArray* result = [originalArray arrayByRemovingObjectsFromArray:itemsToRemove];
+    
+    XCTAssert(resultArray.count == 5);
+    XCTAssert([resultArray containsObject:@1]);
+    XCTAssert([resultArray containsObject:@2]);
+    XCTAssert([resultArray containsObject:@3]);
+    XCTAssert([resultArray containsObject:@4]);
+    XCTAssert([resultArray containsObject:@5]);
+}
+
 @end
