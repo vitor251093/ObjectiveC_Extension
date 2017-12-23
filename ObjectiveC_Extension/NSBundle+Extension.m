756670ac7ca37a9b7b406686e6217e72354867bb
Vitor <vitor251093@gmail.com>
2017-12-23 15:11:39 -0200

    bundleName and bundlePathBeforeAppTranslocation now are stored properly

--- ObjectiveC_Extension/NSBundle+Extension.m
+++ ObjectiveC_Extension/NSBundle+Extension.m
@@ -13,6 +13,10 @@
 #import "VMMComputerInformation.h"
 
 #include <stdlib.h>
+#import <objc/runtime.h>
+
+static char NSBundleBundleNameKey;
+static char NSBundleBundlePathBeforeAppTranslocationKey;
 
 @implementation NSBundle (VMMBundle)
 
@@ -20,31 +24,40 @@ NSBundle* _originalMainBundle;
 
 -(nonnull NSString*)bundleName
 {
-    NSString* bundleName;
-    
-    bundleName = [self objectForInfoDictionaryKey:@"CFBundleDisplayName"];
-    
-    if (bundleName == nil)
+    @synchronized (self)
     {
-        bundleName = [self objectForInfoDictionaryKey:@"CFBundleName"];
-    }
-    
-    if (bundleName == nil)
-    {
-        // Reference:
-        // https://stackoverflow.com/a/35322073/4370893
+        NSString* bundleName = (NSString*)objc_getAssociatedObject(self, &NSBundleBundleNameKey);
+        if (bundleName != nil) return bundleName;
         
-        bundleName = [NSString stringWithUTF8String:getprogname()];
+        bundleName = [self objectForInfoDictionaryKey:@"CFBundleDisplayName"];
+        
+        if (bundleName == nil)
+        {
+            bundleName = [self objectForInfoDictionaryKey:@"CFBundleName"];
+        }
+        
+        if (bundleName == nil)
+        {
+            // Reference:
+            // https://stackoverflow.com/a/35322073/4370893
+            
+            bundleName = [NSString stringWithUTF8String:getprogname()];
+        }
+        
+        if (bundleName == nil)
+        {
+            NSString* placeholder = @"App";
+            NSString* bundlePath = [self bundlePath];
+            bundleName = bundlePath ? bundlePath.stringByDeletingPathExtension.lastPathComponent : placeholder;
+        }
+        
+        if (bundleName != nil)
+        {
+            objc_setAssociatedObject(self, &NSBundleBundleNameKey, bundleName, OBJC_ASSOCIATION_ASSIGN);
+        }
+        
+        return bundleName;
     }
-    
-    if (bundleName == nil)
-    {
-        NSString* placeholder = @"App";
-        NSString* bundlePath = [self bundlePath];
-        bundleName = bundlePath ? bundlePath.stringByDeletingPathExtension.lastPathComponent : placeholder;
-    }
-    
-    return bundleName;
 }
 
 -(BOOL)isAppTranslocationActive
@@ -68,74 +81,95 @@ NSBundle* _originalMainBundle;
 }
 -(NSString*)bundlePathBeforeAppTranslocation
 {
-    NSString* bundlePathBeforeAppTranslocation;
-    
-    if (bundlePathBeforeAppTranslocation != nil && [[NSFileManager defaultManager] fileExistsAtPath:bundlePathBeforeAppTranslocation])
+    @synchronized (self)
     {
-        return bundlePathBeforeAppTranslocation;
-    }
-    
-    NSString* appBinaryPath = [self executablePath];
-    NSString* appBinaryChecksum = [[NSFileManager defaultManager] checksum:NSChecksumTypeMD5 ofFileAtPath:appBinaryPath];
-    NSString* appBundleFileName = [[self bundlePath] lastPathComponent];
-    if (!appBinaryChecksum || !appBundleFileName) return nil;
-    
-    NSString* applicationsFolderPath = @"/Applications/";
-    NSString* desktopFolderPath = [NSString stringWithFormat:@"%@/Desktop/",NSHomeDirectory()];
-    NSString* downloadsFolderPath = [NSString stringWithFormat:@"%@/Downloads/",NSHomeDirectory()];
-    
-    NSString* applicationInDesktopFolderPath = [desktopFolderPath stringByAppendingString:appBundleFileName];
-    if ([self doesBundleAtPath:applicationInDesktopFolderPath executableMatchesWithMD5Checksum:appBinaryChecksum])
-    {
-        bundlePathBeforeAppTranslocation = applicationInDesktopFolderPath;
-        return bundlePathBeforeAppTranslocation;
-    }
-    
-    NSString* applicationInDownloadsFolderPath = [downloadsFolderPath stringByAppendingString:appBundleFileName];
-    if ([self doesBundleAtPath:applicationInDownloadsFolderPath executableMatchesWithMD5Checksum:appBinaryChecksum])
-    {
-        bundlePathBeforeAppTranslocation = applicationInDownloadsFolderPath;
-        return bundlePathBeforeAppTranslocation;
-    }
-    
-    NSString* applicationInApplicationsFolderPath = [applicationsFolderPath stringByAppendingString:appBundleFileName];
-    if ([self doesBundleAtPath:applicationInApplicationsFolderPath executableMatchesWithMD5Checksum:appBinaryChecksum])
-    {
-        bundlePathBeforeAppTranslocation = applicationInApplicationsFolderPath;
-        return bundlePathBeforeAppTranslocation;
-    }
-    
-    NSArray* desktopFilesMatches = [[NSFileManager defaultManager] subpathsAtPath:desktopFolderPath ofFilesNamed:appBundleFileName];
-    for (NSString* desktopFilesMatch in desktopFilesMatches)
-    {
-        if ([self doesBundleAtPath:desktopFilesMatch executableMatchesWithMD5Checksum:appBinaryChecksum])
+        NSString* bundlePathBeforeAppTranslocation = (NSString*)objc_getAssociatedObject(self, &NSBundleBundlePathBeforeAppTranslocationKey);
+        if (bundlePathBeforeAppTranslocation != nil && [[NSFileManager defaultManager] fileExistsAtPath:bundlePathBeforeAppTranslocation])
         {
-            bundlePathBeforeAppTranslocation = desktopFilesMatch;
             return bundlePathBeforeAppTranslocation;
         }
-    }
-    
-    NSArray* downloadsFilesMatches = [[NSFileManager defaultManager] subpathsAtPath:downloadsFolderPath ofFilesNamed:appBundleFileName];
-    for (NSString* downloadsFilesMatch in downloadsFilesMatches)
-    {
-        if ([self doesBundleAtPath:downloadsFilesMatch executableMatchesWithMD5Checksum:appBinaryChecksum])
+        
+        NSString* appBinaryPath = [self executablePath];
+        NSString* appBinaryChecksum = [[NSFileManager defaultManager] checksum:NSChecksumTypeMD5 ofFileAtPath:appBinaryPath];
+        NSString* appBundleFileName = [[self bundlePath] lastPathComponent];
+        if (!appBinaryChecksum || !appBundleFileName) return nil;
+        
+        NSString* applicationsFolderPath = @"/Applications/";
+        NSString* desktopFolderPath = [NSString stringWithFormat:@"%@/Desktop/",NSHomeDirectory()];
+        NSString* downloadsFolderPath = [NSString stringWithFormat:@"%@/Downloads/",NSHomeDirectory()];
+        
+        NSString* applicationInDesktopFolderPath = [desktopFolderPath stringByAppendingString:appBundleFileName];
+        if ([self doesBundleAtPath:applicationInDesktopFolderPath executableMatchesWithMD5Checksum:appBinaryChecksum])
         {
-            bundlePathBeforeAppTranslocation = downloadsFilesMatch;
-            return bundlePathBeforeAppTranslocation;
+            bundlePathBeforeAppTranslocation = applicationInDesktopFolderPath;
         }
-    }
-    
-    NSArray* applicationsFilesMatches = [[NSFileManager defaultManager] subpathsAtPath:applicationsFolderPath ofFilesNamed:appBundleFileName];
-    for (NSString* applicationsFilesMatch in applicationsFilesMatches)
-    {
-        if ([self doesBundleAtPath:applicationsFilesMatch executableMatchesWithMD5Checksum:appBinaryChecksum])
+        
+        if (bundlePathBeforeAppTranslocation == nil)
         {
-            bundlePathBeforeAppTranslocation = applicationsFilesMatch;
-            return bundlePathBeforeAppTranslocation;
+            NSString* applicationInDownloadsFolderPath = [downloadsFolderPath stringByAppendingString:appBundleFileName];
+            if ([self doesBundleAtPath:applicationInDownloadsFolderPath executableMatchesWithMD5Checksum:appBinaryChecksum])
+            {
+                bundlePathBeforeAppTranslocation = applicationInDownloadsFolderPath;
+            }
         }
+        
+        if (bundlePathBeforeAppTranslocation == nil)
+        {
+            NSString* applicationInApplicationsFolderPath = [applicationsFolderPath stringByAppendingString:appBundleFileName];
+            if ([self doesBundleAtPath:applicationInApplicationsFolderPath executableMatchesWithMD5Checksum:appBinaryChecksum])
+            {
+                bundlePathBeforeAppTranslocation = applicationInApplicationsFolderPath;
+            }
+        }
+        
+        if (bundlePathBeforeAppTranslocation == nil)
+        {
+            NSArray* desktopFilesMatches = [[NSFileManager defaultManager] subpathsAtPath:desktopFolderPath ofFilesNamed:appBundleFileName];
+            for (NSString* desktopFilesMatch in desktopFilesMatches)
+            {
+                if ([self doesBundleAtPath:desktopFilesMatch executableMatchesWithMD5Checksum:appBinaryChecksum])
+                {
+                    bundlePathBeforeAppTranslocation = desktopFilesMatch;
+                    break;
+                }
+            }
+        }
+        
+        if (bundlePathBeforeAppTranslocation == nil)
+        {
+            NSArray* downloadsFilesMatches = [[NSFileManager defaultManager] subpathsAtPath:downloadsFolderPath ofFilesNamed:appBundleFileName];
+            for (NSString* downloadsFilesMatch in downloadsFilesMatches)
+            {
+                if ([self doesBundleAtPath:downloadsFilesMatch executableMatchesWithMD5Checksum:appBinaryChecksum])
+                {
+                    bundlePathBeforeAppTranslocation = downloadsFilesMatch;
+                    break;
+                }
+            }
+        }
+        
+        if (bundlePathBeforeAppTranslocation == nil)
+        {
+            NSArray* applicationsFilesMatches = [[NSFileManager defaultManager] subpathsAtPath:applicationsFolderPath
+                                                                                  ofFilesNamed:appBundleFileName];
+            for (NSString* applicationsFilesMatch in applicationsFilesMatches)
+            {
+                if ([self doesBundleAtPath:applicationsFilesMatch executableMatchesWithMD5Checksum:appBinaryChecksum])
+                {
+                    bundlePathBeforeAppTranslocation = applicationsFilesMatch;
+                    break;
+                }
+            }
+        }
+        
+        if (bundlePathBeforeAppTranslocation != nil)
+        {
+            objc_setAssociatedObject(self, &NSBundleBundlePathBeforeAppTranslocationKey,
+                                     bundlePathBeforeAppTranslocation, OBJC_ASSOCIATION_ASSIGN);
+        }
+        
+        return bundlePathBeforeAppTranslocation;
     }
-    
-    return nil;
 }
 
 +(nullable NSBundle*)originalMainBundle
