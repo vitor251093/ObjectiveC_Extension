3c1676e58a84c057b3110f0464d6494095340472
Vitor Marques de Miranda <vitor251093@yahoo.com.br>
2018-03-27 10:26:22 -0300

    NSArray extension improved to show the ObjectType in the auto complete

--- ObjectiveC_Extension/NSArray+Extension.h
+++ ObjectiveC_Extension/NSArray+Extension.h
@@ -8,16 +8,16 @@
 
 #import <Cocoa/Cocoa.h>
 
-@interface NSArray (VMMArray)
+@interface NSArray<ObjectType> (VMMArray)
 
--(nonnull NSArray*)arrayByRemovingRepetitions;
--(nonnull NSArray*)arrayByRemovingObjectsFromArray:(nonnull NSArray*)otherArray;
+-(nonnull NSArray<ObjectType>*)arrayByRemovingRepetitions;
+-(nonnull NSArray<ObjectType>*)arrayByRemovingObjectsFromArray:(nonnull NSArray<ObjectType>*)otherArray;
 
--(NSIndexSet* _Nonnull)indexesOfObject:(id _Nonnull)object inRange:(NSRange)range;
--(NSIndexSet* _Nonnull)indexesOfObject:(id _Nonnull)object;
+-(NSIndexSet* _Nonnull)indexesOfObject:(ObjectType _Nonnull)object inRange:(NSRange)range;
+-(NSIndexSet* _Nonnull)indexesOfObject:(ObjectType _Nonnull)object;
 
--(NSInteger)lastIndexOfObject:(id _Nonnull)object inRange:(NSRange)range;
+-(NSInteger)lastIndexOfObject:(ObjectType _Nonnull)object inRange:(NSRange)range;
 
--(void)differencesFromArray:(NSArray* _Nonnull)otherArray indexPaths:(void (^_Nonnull)(NSArray<NSIndexSet*>* _Nonnull addedIndexes, NSArray<NSIndexSet*>* _Nonnull removedIndexes))indexPaths;
+-(void)differencesFromArray:(NSArray<ObjectType>* _Nonnull)otherArray indexPaths:(void (^_Nonnull)(NSArray<NSIndexSet*>* _Nonnull addedIndexes, NSArray<NSIndexSet*>* _Nonnull removedIndexes))indexPaths;
 
 @end
