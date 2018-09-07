11c153edc9d9e3a900600640b0f025f156c0613f
Vitor <vitor251093@gmail.com>
2018-09-07 14:30:04 -0300

    Added TODO of required bug fix

--- ObjectiveC_Extension/VMMDeviceSimulator.m
+++ ObjectiveC_Extension/VMMDeviceSimulator.m
@@ -15,6 +15,13 @@
 
 +(void)simulateCursorClickAtScreenPoint:(CGPoint)clickPoint
 {
+    // TODO: In macOS Mojave, the method below requires accessibility permissions.
+    // https://objective-see.com/blog/blog_0x36.html
+    // https://forums.developer.apple.com/thread/105667
+    //
+    // Still couldn't find a suitable workaround for requiring the permission
+    // a second time in case the user denied by mistake.
+
     CGEventRef theEvent = CGEventCreateMouseEvent(NULL, kCGEventLeftMouseDown, clickPoint, kCGMouseButtonLeft);
     CGEventPost(kCGHIDEventTap, theEvent);
     CGEventSetType(theEvent, kCGEventLeftMouseUp);
