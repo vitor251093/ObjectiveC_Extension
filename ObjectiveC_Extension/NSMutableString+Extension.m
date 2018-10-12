44e747d4e08a05bc9ad024852276990c62d0fbf6
Vitor <vitor251093@gmail.com>
2018-10-12 00:39:33 -0300

    Improved trim function so it's much faster by using less Objective-C functions

--- ObjectiveC_Extension/NSMutableString+Extension.m
+++ ObjectiveC_Extension/NSMutableString+Extension.m
@@ -16,10 +16,53 @@
 }
 
 -(void)trim {
-    while ([self hasPrefix:@" "] || [self hasPrefix:@"\n"] || [self hasPrefix:@"\t"])
-        [self replaceCharactersInRange:NSMakeRange(0, 1) withString:@""];
-    while ([self hasSuffix:@" "] || [self hasSuffix:@"\n"] || [self hasSuffix:@"\t"])
-        [self replaceCharactersInRange:NSMakeRange(self.length-1, 1) withString:@""];
+    // We used to use the method below, but the method in use today is about 2 to 5 times faster
+    
+    //    while ([self hasPrefix:@" "] || [self hasPrefix:@"\n"] || [self hasPrefix:@"\t"])
+    //        [self replaceCharactersInRange:NSMakeRange(0, 1) withString:@""];
+    //    while ([self hasSuffix:@" "] || [self hasSuffix:@"\n"] || [self hasSuffix:@"\t"])
+    //        [self replaceCharactersInRange:NSMakeRange(self.length-1, 1) withString:@""];
+    
+    NSUInteger stringLength = self.length;
+    if (stringLength == 0) return;
+    
+    NSUInteger indexBegin = 0;
+    unichar invalidCharacters[] = {' ', '\n', '\t', '\0'};
+    int invalidCharactersSize = 4;
+    
+    unichar actualChar;
+    int charIndex;
+    BOOL invalid;
+    while (indexBegin < stringLength) {
+        invalid = false;
+        actualChar = [self characterAtIndex:indexBegin];
+        for (charIndex = 0; charIndex < invalidCharactersSize; charIndex++) {
+            if (invalidCharacters[charIndex] == actualChar) invalid = true;
+        }
+        if (invalid) indexBegin++;
+        else break;
+    }
+    if (indexBegin > 0) {
+        [self replaceCharactersInRange:NSMakeRange(0, indexBegin) withString:@""];
+    }
+    if (indexBegin == stringLength) return;
+    
+    
+    stringLength = self.length;
+    NSUInteger indexEnd = stringLength - 1;
+    
+    while (indexEnd >= 0) {
+        invalid = false;
+        actualChar = [self characterAtIndex:indexEnd];
+        for (charIndex = 0; charIndex < invalidCharactersSize; charIndex++) {
+            if (invalidCharacters[charIndex] == actualChar) invalid = true;
+        }
+        if (invalid) indexEnd--;
+        else break;
+    }
+    if (stringLength - indexEnd - 1 > 0) {
+        [self replaceCharactersInRange:NSMakeRange(indexEnd + 1, stringLength - indexEnd - 1) withString:@""];
+    }
 }
 
 @end
